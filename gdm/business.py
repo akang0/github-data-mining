240751432b0045660426363d5c6da78bb8e45987
Xuning Kang <xkang@redhat.com>
2021-03-25 02:27:50 +0800

    Add new args in mining

--- gdm/business.py
+++ gdm/business.py
@@ -42,3 +42,38 @@ for repo in db["repos"].find():
             print("upserting... ", repo_json)
             db["repos"].update_one({"id": repo["id"]}, pr_repo_set, upsert=True)
             print("done upsert: ", repo_json)
+
+# Sort out labels of issue from github raw data
+qe_member = ["quchangl-github", "drkho", "thuyn-581", "laisongls", "akang0", "dhrpatel4", "evelinec"]
+for issue in db["repos_issues"].find():
+    labels = issue["labels"]
+    labelsArray = labels.split(',')
+    # Add bug label
+    if 'bug' in labelsArray:
+        issue["isBug"] = "True"
+    # Add squad label
+    if any('squad' in s for s in labelsArray):
+        squadMatching = [s for s in labelsArray if 'squad' in s]
+        if len(squadMatching) == 1:
+            issue["squad"] = squadMatching[0]
+    # Add QE created label
+    creator = issue["createdBy"]
+    if creator in qe_member:
+        issue["QEcreated"] = "QE"
+    else:
+        issue["QEcreated"] = "Non-QE"
+    # Add priority label
+    if 'blocker (P0)' in labelsArray:
+        issue["priority"] = 'Priority/P0'
+    elif any('Priority' in s for s in labelsArray):
+        priorityMatching = [s for s in labelsArray if 'Priority' in s]
+        issue["priority"] = priorityMatching[0]
+    # Update DB
+    issue_set = {"$set": issue}
+    print("upserting... ", issue)
+    db["repos_issues"].update_one(
+        {"id": issue["id"]},
+        issue_set,
+        upsert=True,
+    )
+    print("done upsert: ", issue)
--- gdm/mine.py
+++ gdm/mine.py
@@ -41,65 +41,76 @@ for repo in org.get_repos():
         repo_json["type"] = "public"
 
     repo_json_set = {"$set": repo_json}
-    print("upserting... ", repo_json)
+    print("upserting.... ", repo_json)
     db["repos"].update_one({"id": repo.id}, repo_json_set, upsert=True)
     print("done upsert: ", repo_json)
 
-    # get all the prs under the repo and upsert to the db.repos_pulls
-    for pr in repo.get_pulls(state="all"):
-        pr_json = {
-            "repoId": repo.id,
-            "repoName": repo.name,
-            "id": pr.id,
-            "title": pr.title,
-            "state": pr.state,
-            "createdAt": pr.created_at.strftime("%Y-%m-%d"),
-        }
+    if repo.name=="backlog":
+        # get all the prs under the repo and upsert to the db.repos_pulls
+        for pr in repo.get_pulls(state="all"):
+            pr_json = {
+                "repoId": repo.id,
+                "repoName": repo.name,
+                "id": pr.id,
+                "title": pr.title,
+                "state": pr.state,
+                "createdAt": pr.created_at.strftime("%Y-%m-%d"),
+            }
 
-        if pr.closed_at is None:
-            age = (today - pr.created_at).days
-            if age < 0:
-                age = 0
-            pr_json["age"] = age
-        else:
-            pr_json["closedAt"] = pr.closed_at.strftime("%Y-%m-%d")
-            daysToClose = (pr.closed_at - pr.created_at).days
-            if daysToClose < 0:
-                daysToClose = 0
-            pr_json["daysToClose"] = daysToClose
-            pr_json["age"] = pr_json["daysToClose"]
+            if pr.closed_at is None:
+                age = (today - pr.created_at).days
+                if age < 0:
+                    age = 0
+                pr_json["age"] = age
+            else:
+                pr_json["closedAt"] = pr.closed_at.strftime("%Y-%m-%d")
+                daysToClose = (pr.closed_at - pr.created_at).days
+                if daysToClose < 0:
+                    daysToClose = 0
+                pr_json["daysToClose"] = daysToClose
+                pr_json["age"] = pr_json["daysToClose"]
 
-        pr_json_set = {"$set": pr_json}
-        print("upserting... ", pr_json)
-        db["repos_pulls"].update_one(
-            {"id": pr.id},
-            pr_json_set,
-            upsert=True,
-        )
-        print("done upsert: ", pr_json)
+            pr_json_set = {"$set": pr_json}
+            print("upserting... ", pr_json)
+            db["repos_pulls"].update_one(
+                {"id": pr.id},
+                pr_json_set,
+                upsert=True,
+            )
+            print("done upsert: ", pr_json)
 
-    # get all the issues under the repo and upsert to the db.repos_issues
-    for issue in repo.get_issues(state="all"):
-        issue_json = {
-            "repoId": repo.id,
-            "repoName": repo.name,
-            "id": issue.id,
-            "number": issue.number,
-            "title": issue.title,
-            "body": issue.body,
-            "state": issue.state,
-            "createdAt": issue.created_at.strftime("%Y-%m-%d"),
-        }
+        # get all the issues under the repo and upsert to the db.repos_issues
+        for issue in repo.get_issues(state="all"):
+            labels = ""
+            milestone = None
+            for label in issue.get_labels():
+                labels = labels + ","
+                labels = labels + label.name
+            if issue.milestone:
+                milestone = issue.milestone.title
+            issue_json = {
+                "repoId": repo.id,
+                "repoName": repo.name,
+                "id": issue.id,
+                "number": issue.number,
+                "createdBy": issue.user.login,
+                "title": issue.title,
+                "body": issue.body,
+                "state": issue.state,
+                "createdAt": issue.created_at.strftime("%Y-%m-%d"),
+                "labels": labels,
+                "milestone": milestone
+            }
 
-        if issue.closed_at is not None:
-            issue_json["closedAt"] = issue.closed_at.strftime("%Y-%m-%d")
-            issue_json["daysToClose"] = (issue.closed_at - issue.created_at).days
+            if issue.closed_at is not None:
+                issue_json["closedAt"] = issue.closed_at.strftime("%Y-%m-%d")
+                issue_json["daysToClose"] = (issue.closed_at - issue.created_at).days
 
-        issue_set = {"$set": issue_json}
-        print("upserting... ", issue_json)
-        db["repos_issues"].update_one(
-            {"id": issue.id},
-            issue_set,
-            upsert=True,
-        )
-        print("done upsert: ", issue_json)
+            issue_set = {"$set": issue_json}
+            print("upserting... ", issue_json)
+            db["repos_issues"].update_one(
+                {"id": issue.id},
+                issue_set,
+                upsert=True,
+            )
+            print("done upsert: ", issue_json)
